1719a00cd86d621c8edf71b5e5e2858fa33708ed
Mike <aeonlucid@outlook.com>
2018-04-28 04:10:57 +0200

    Add Osx x86 native library.

---
+++ SteamworksSharp.Native.Osx_x86/NativeLibrary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using SteamworksSharp.Native.Libs;
+
+namespace SteamworksSharp.Native.Osx_x86
+{
+    public class NativeLibrary : INativeLibrary
+    {
+        public OSPlatform Platform { get; } = OSPlatform.OSX;
+
+        public Architecture Architecture { get; } = Architecture.X86;
+
+        public Lazy<byte[]> LibraryBytes { get; } = new Lazy<byte[]>(() => 
+            LibUtils.ReadResourceBytes(typeof(NativeLibrary).GetTypeInfo().Assembly, "SteamworksSharp.Native.Linux_x86.libsteam_api.dylib"));
+
+        public string Extension { get; } = "dylib";
+    }
+}
--- SteamworksSharp.Native/AssemblyInfo.cs
+++ SteamworksSharp.Native/AssemblyInfo.cs
@@ -4,3 +4,4 @@
 [assembly: InternalsVisibleTo("SteamworksSharp.Native.Linux_x64")]
 [assembly: InternalsVisibleTo("SteamworksSharp.Native.Windows_x86")]
 [assembly: InternalsVisibleTo("SteamworksSharp.Native.Windows_x64")]
+[assembly: InternalsVisibleTo("SteamworksSharp.Native.Osx_x86")]
